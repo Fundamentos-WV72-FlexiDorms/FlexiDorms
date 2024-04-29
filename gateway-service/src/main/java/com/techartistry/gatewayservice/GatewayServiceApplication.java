ca30c148abc8ef70f003b25cf5e6d136dff263fc
J. Ortega <jamutaqortegavelez@gmail.com>
2024-04-29 12:23:09 -0500

    add: config for gateway-service & banner
    Co-Authored-By: Anthony Piero Avalos Santos <104176063+Anthony2000AA@users.noreply.github.com>
    Co-Authored-By: JosArr <77064719+JosArr@users.noreply.github.com>
    Co-Authored-By: XLianLZ <104079538+XLianLZ@users.noreply.github.com>

--- gateway-service/src/main/java/com/techartistry/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/techartistry/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,12 @@ package com.techartistry.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient //se indica que es un cliente de eureka
 @SpringBootApplication
 public class GatewayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
 }
