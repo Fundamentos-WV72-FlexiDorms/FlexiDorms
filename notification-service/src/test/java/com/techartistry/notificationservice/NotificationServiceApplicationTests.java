f07e304b9b9560f5f50d8d67ad68412aca41f17e
J. Ortega <jamutaqortegavelez@gmail.com>
2024-05-04 19:40:24 -0500

    feat: add base project for notification-service

---
+++ notification-service/src/test/java/com/techartistry/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techartistry.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
