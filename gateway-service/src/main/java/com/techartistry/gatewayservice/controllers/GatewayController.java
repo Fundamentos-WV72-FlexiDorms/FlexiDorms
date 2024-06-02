60de4c50c53d3dee61d04d87893ba54025200f15
J. Ortega <jamutaqortegavelez@gmail.com>
2024-06-02 01:32:48 -0500

    update: add logout in gateway

--- gateway-service/src/main/java/com/techartistry/gatewayservice/controllers/GatewayController.java
+++ gateway-service/src/main/java/com/techartistry/gatewayservice/controllers/GatewayController.java
@@ -1,7 +1,9 @@
 package com.techartistry.gatewayservice.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.WebSession;
@@ -14,8 +16,9 @@ public class GatewayController {
      * @param session Sesión
      */
     @GetMapping("/")
-    public Mono<String> index(WebSession session) {
-        return Mono.just(session.getId());
+    public Mono<String> index(WebSession session, @AuthenticationPrincipal DefaultOidcUser principal) {
+        String username = principal.getPreferredUsername();
+        return Mono.just("Hello, " + username + "!<br/> Session: <code>" + session.getId() + "</code>");
     }
 
     /**
@@ -23,10 +26,7 @@ public class GatewayController {
      * @param authorizedClient Cliente autorizado
      */
     @GetMapping("/token")
-    public Mono<String> getToken(
-            @RegisteredOAuth2AuthorizedClient
-            OAuth2AuthorizedClient authorizedClient
-    ) {
+    public Mono<String> getToken(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
         return Mono.just(authorizedClient.getAccessToken().getTokenValue());
     }
 }
--- gateway-service/src/main/java/com/techartistry/gatewayservice/security/SecurityConfig.java
+++ gateway-service/src/main/java/com/techartistry/gatewayservice/security/SecurityConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,13 +17,28 @@ public class SecurityConfig {
      * Intercepta todas las solicitudes entrantes y asegura que el usuario esté autenticado.
      */
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, ServerLogoutSuccessHandler logoutSuccessHandler) {
         http.authorizeExchange(exchanges -> exchanges
             .anyExchange().authenticated()
         );
         http.oauth2Login(Customizer.withDefaults());
+        http.logout(logout -> logout
+            .logoutSuccessHandler(logoutSuccessHandler)
+        );
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return http.build();
+    }
+
+    /**
+     * Se crea un controlador de logout del servidor iniciado por el cliente.
+     * Esto inicia el cierre de sesión del usuario en el servidor de autorización (Keycloak).
+     * @param clientRegistrationRepository Repositorio de registro de clientes
+     */
+    @Bean
+    public ServerLogoutSuccessHandler logoutSuccessHandler(ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        var successHandler = new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("{baseUrl}/login?logout");
+        return successHandler;
     }
 }
